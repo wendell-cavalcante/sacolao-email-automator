92238a7c9f3508d62dfd5c27cf8c05b97f9459db
Wendell Cavalcante <wendell160326@gmail.com>
2026-08-27 15:41:00 -0300

    feat & fix: fixed some bugs and the log erro if the attachments theres some error

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@
 const GOOGLE_CLIENT_ID = "77948638954-066c13a98i0puussbbpfjdr7qnohhmrh.apps.googleusercontent.com";
 
 const STORE_EMAILS = {
-  lapa:     "wendell160326@gmail.com",
+  lapa:     "hemeson.jasp@gmail.com, jvitorlima19@hotmail.com, leandrosaclapa@yahoo.com.br, givanildo.sacolaohigienopolis@gmail.com, emillysacolao@gmail.com, josicarlaj8@gmail.com, adrianacosta.sacolaolapa@gmail.com, joao.bernardi.sacolao@gmail.com, biondisacolao@gmail.com",
   hig:      "hemeson.jasp@gmail.com, lara.higienopolis@gmail.com, laurasacolao4@gmail.com, Thiago@sacolaohigienopolis.com.br, leandrosaclapa@yahoo.com.br, joao.bernardi.sacolao@gmail.com, biondisacolao@gmail.com, givanildo.sacolaohigienopolis@gmail.com",
   ce:       "hemeson.jasp@gmail.com, cleiane_nani@hotmail.com, juliana.sacolaocampos@gmail.com, felipesacolaocamposeliseos@gmail.com, joao.bernardi.sacolao@gmail.com, leandrosaclapa@yahoo.com.br, biondisacolao@gmail.com, givanildo.sacolaohigienopolis@gmail.com",
   perdizes: "hemeson.jasp@gmail.com, atendimento@sacolaoperdizes.com.br, jennyfferperdizes@gmail.com, leandrosaclapa@yahoo.com.br, joao.bernardi.sacolao@gmail.com, biondisacolao@gmail.com, givanildo.sacolaohigienopolis@gmail.com, Iagoperdizes0@gmail.com",
@@ -17,4 +17,12 @@ const STORE_NAMES = {
   hig:      "Hig",
   ce:       "Campos",
   perdizes: "Perdizes",
+};
+
+// Palavras-chave permitidas nos nomes dos arquivos para cada loja
+const STORE_KEYWORDS = {
+  lapa:     ["lapa"],
+  hig:      ["hig", "higienopolis", "higienópolis"],
+  ce:       ["ce", "campos", "eliseos", "elíseos", "c.e", "c.e."],
+  perdizes: ["perdizes", "perdi", "perd"],
 };
